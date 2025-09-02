e66da71eb90d1f4666043e1f79ec4bac4e5cd834
Cody Platt <62489180+plattco@users.noreply.github.com>
2025-09-02 14:50:57 -0400

    implementing vowel reversal algo

---
+++ CSAlgorithms/VowelReversal.cs
@@ -0,0 +1,57 @@
+namespace DefaultNamespace;
+
+public class VowelReversal
+{
+    public string ReverseVowels(string s)
+    {
+        int i = 0;
+        string vowels = "";
+        List<int> vowelIndexes = new List<int>();
+        char[] charArray = s.ToCharArray();
+        // loop through string
+        while (i < s.Length)
+        {
+            // store vowels in list
+            // store vowel indeces in a list
+            if (IsVowel(s[i]))
+            {
+                vowels += s[i];
+                vowelIndexes.Add(i);
+            }
+
+            i++;
+        }
+
+        i = 0;
+        // loop through string again
+        while (i < s.Length)
+        {
+            // place new vowels where they were reversed
+            if (vowelIndexes.Contains(i))
+            {
+                charArray[i] = vowels[vowels.Length - 1];
+                vowels = vowels.Substring(0, vowels.Length - 1);
+            }
+
+            i++;
+        }
+        
+        return new string(charArray);
+    }
+
+    public bool IsVowel(char c)
+    {
+        char lowerC = char.ToLower(c);
+        switch (lowerC)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                return true;
+            default:
+                return false;
+        }
+    }
+}
