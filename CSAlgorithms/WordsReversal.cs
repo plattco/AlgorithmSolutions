327107e23cf4536736d3700f1b183ea4af1af88d
Cody Platt <62489180+plattco@users.noreply.github.com>
2025-09-02 22:38:56 -0400

    Creating function and solution logic

---
+++ CSAlgorithms/WordsReversal.cs
@@ -0,0 +1,16 @@
+namespace DefaultNamespace;
+
+public class WordsReversal
+{
+    public string ReverseWords(string s) 
+    {
+        // store a new string to begin reversal
+        // loop through the string, backwards
+            // store a temp "word"
+            // start adding chars to temp word
+            // when we reach a space, reverse the word and add it to the reversed string above
+            // clear out temp word
+            // rinse and repeat
+        // return words
+    }
+}
