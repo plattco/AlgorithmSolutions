78b88fd691089f83b024ab99956ae52a33ee7f10
Cody Platt <62489180+plattco@users.noreply.github.com>
2025-09-03 18:00:12 -0400

    Finished reversal solution

--- CSAlgorithms/WordsReversal.cs
+++ CSAlgorithms/WordsReversal.cs
@@ -5,13 +5,36 @@ public class WordsReversal
     public string ReverseWords(string s) 
     {
         // store a new string to begin reversal
+		string reversed = "";
         // loop through the string, backwards
-            // store a temp "word"
-            // start adding chars to temp word
-            // when we reach a space, reverse the word and add it to the reversed string above
-            // make sure spaces are clean
-            // clear out temp word
-            // rinse and repeat
+        // store a temp "word"
+		string word = "";
+		for (int i = (s.Length-1); i >= 0; i--)
+		{
+            if (s[i] == ' ')
+            {
+                if (word.Length > 0)
+                {
+                    reversed += ReverseWord(word) + " ";
+                    word = "";
+                }
+            }
+            else
+            {
+                word += s[i];
+            }
+		}
+        // handle last word
+        reversed += ReverseWord(word);
+            
         // return words
+		return reversed.Trim();
+    }
+
+    public string ReverseWord(string word)
+    {
+        char[] charArray = word.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
     }
 }
